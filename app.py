368cc5aff6865218c5df46b29b2f05fa1170be99
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2022-12-05 19:12:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,32 @@ if db_conf == 'Yes':
             finally:
                 cur.close()
         con.close()
-        db_conf = st.radio("Do you want to create Schema",('Yes', 'No'))
+        sc_conf = st.radio("Do you want to create Schema",('No', 'Yes'))
+        if sc_conf == 'Yes':
+            sc_name = st.text_input('Enter Schema Name')
+            if st.button('submit'):
+                sql_cmd1 = 'CREATE OR REPLACE SCHEMA ' + str(sc_name) + ';'
+                with snowflake.connector.connect(
+                        user = user,
+                        password = password,
+                        account='VK83964.ap-southeast-1',
+                        warehouse='DNAHACK', 
+                        database = db_name) as con:
+                    
+                    try:
+                        cur = con.cursor()
+                        cur.execute(sql_cmd)
+                        st.write('Database has been created')
+                    except Exception as e:
+                        print(e)
+                        st.write('An error has occured please check logs')
+                    finally:
+                        cur.close()
+                con.close()
+                
+                
+            
+
         
     
 
